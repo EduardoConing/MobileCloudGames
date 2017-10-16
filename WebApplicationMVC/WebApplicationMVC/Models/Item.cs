9e4290192a21fe7aba09578b02c4d8797cb71a1b
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-10-16 17:17:33 -0200

    Versao inicial do projeto MVC

---
+++ WebApplicationMVC/WebApplicationMVC/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplicationMVC.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int Preco { get; set; }
+
+    }
+}
---
+++ WebApplicationMVC/WebApplicationMVC/Models/MeuContexto.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace WebApplicationMVC.Models
+{
+    public class MeuContexto : DbContext
+    {
+        // Construtor
+        public MeuContexto() : base("strConexao")
+        {
+
+        }
+
+        public DbSet<Item> Itens { get; set; }
+    }
+}
