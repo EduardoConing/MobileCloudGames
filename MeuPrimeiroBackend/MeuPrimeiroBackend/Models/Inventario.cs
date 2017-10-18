f7690fb0c38d086799e5c2926041d2417a316abc
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-10-18 17:00:30 -0200

    Adicionando models Jogador e Inventario

---
+++ MeuPrimeiroBackend/MeuPrimeiroBackend/Models/Inventario.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MeuPrimeiroBackend.Models
+{
+    public class Inventario
+    {
+        public int InventarioID { get; set; }
+
+        public int JogadorID { get; set; }
+
+        public virtual Jogador _Jogador { get; set; }
+
+        public ICollection<Item> ItensInventario { get; set; }
+
+    }
+}
---
+++ MeuPrimeiroBackend/MeuPrimeiroBackend/Models/Jogador.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeuPrimeiroBackend.Models
+{
+    public class Jogador
+    {
+        [Key]
+        public int JogadorID { get; set; }
+
+        public string Nickname { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
