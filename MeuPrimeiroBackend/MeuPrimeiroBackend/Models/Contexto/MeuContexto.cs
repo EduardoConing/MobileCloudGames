74eabb6ce155f9368706873a7298d591d9e492b7
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-10-18 16:26:59 -0200

    Ajustando o contexto para trabalhar com a string de conexão e com o Entity Framework

--- MeuPrimeiroBackend/MeuPrimeiroBackend/Models/Contexto/MeuContexto.cs
+++ MeuPrimeiroBackend/MeuPrimeiroBackend/Models/Contexto/MeuContexto.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace MeuPrimeiroBackend.Models.Contexto
 {
-    public class MeuContexto
+    public class MeuContexto : DbContext
     {
+        public MeuContexto() : base("strConn")
+        {
+
+        }
     }
 }
