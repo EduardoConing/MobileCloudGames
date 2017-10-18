3a9c8026c5d4eee5460ec5cf7c6f7bbbbd20ddbf
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-10-18 16:30:27 -0200

    Adicionando DataAnnotations em Item

--- MeuPrimeiroBackend/MeuPrimeiroBackend/Models/Item.cs
+++ MeuPrimeiroBackend/MeuPrimeiroBackend/Models/Item.cs
@@ -1,15 +1,21 @@
-﻿namespace MeuPrimeiroBackend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeuPrimeiroBackend.Models
 {
     public class Item
     {
         public int ItemID { get; set; }
 
+        [Required, StringLength(20)]
         public string Nome { get; set; }
 
+        [Display(Name ="Descrição")]
         public string Descricao { get; set; }
 
+        [Display(Name ="Dano Máximo")]
         public int DanoMaximo { get; set; }
 
+        [Display(Name ="Dano Mínimo")]
         public int DanoMinimo { get; set; }
 
         public int Durabilidade { get; set; }
